8a1114e65ea0b064585dbb25c6746b9b7d8413b1
Amaury Socorro <Amaury@Amaurys-MacBook-Pro.local>
2021-11-24 21:05:41 -0500

    cellier controller ready

---
+++ app/Http/Controllers/CellierController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Cellier;
+use Illuminate\Http\Request;
+
+class CellierController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Cellier::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        return Cellier::create([
+        'nom_cellier' => $request->nom_cellier,
+        'user_id' => $request->user_id
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Cellier::find($id);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        return Cellier::find($id);
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request)
+    {
+        return Cellier::where('id', $request->id)->update([
+        'nom_cellier' => $request->nom_cellier,
+        'user_id' => $request->user_id
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+       return Cellier::where('id', $id)->delete();
+    }
+}
