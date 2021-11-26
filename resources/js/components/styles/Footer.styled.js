7c45dd826756322db052eae802b79d2911e6309f
unknown <delphinecpicca@gmail.com>
2021-11-26 14:39:05 -0500

    ajout components Layout et Footer

---
+++ resources/js/components/styles/Footer.styled.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const FooterContainer = styled.footer`
+
+    display: flex;
+    background-color: #303031;
+    width: 100%;
+
+    p {
+        padding-top: 10px;
+        color: #fff;
+    }
+    
+
+
+`
