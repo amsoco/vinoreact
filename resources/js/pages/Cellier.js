62577525ac9bb4049f58ded8a01b765c7ac045ec
PaulAntoineGauvreau <paulantoine.gauvreau@gmail.com>
2021-11-29 11:10:56 -0500

    upstream

--- resources/js/pages/Cellier.js
+++ resources/js/pages/Cellier.js
@@ -1,7 +1,11 @@
 import React from "react";
 import Layout from "../components/Layout";
+import { useUser } from "../context/user";
 
 const Cellier = () => {
+    const { user } = useUser();
+    console.log(user);
+
     return <Layout>TOUTES LES BOUTEILLES D'UN CELLIER</Layout>;
 };
 
