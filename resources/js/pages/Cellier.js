d163f48aadc192696747a34a5817783f08918cd6
Pier2208 <p.leroy.mtl@gmail.com>
2021-11-29 17:25:30 -0500

    contexte cellier + page cellier

--- resources/js/pages/Cellier.js
+++ resources/js/pages/Cellier.js
@@ -1,8 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useCellier } from "../context/cellier";
+import { useUser } from "../context/user";
 import Layout from "../components/Layout";
 
 const Cellier = () => {
-    return <Layout>TOUTES LES BOUTEILLES D'UN CELLIER</Layout>;
+    const [bouteilles, setBouteilles] = useState([]);
+    const { getBouteillesCellier } = useCellier();
+    const { user } = useUser();
+
+    useEffect(() => {
+        const { id } = JSON.parse(localStorage.getItem("cellier"));
+        getBouteillesCellier(id).then(({ data }) => {
+            setBouteilles(data)
+        });
+    }, []);
+
+    return (
+        <Layout>
+            <div>
+                <h1>Ton Cellier</h1>
+                <h2>{user?.name}</h2>
+            </div>
+
+            {bouteilles?.map((bouteille) => (
+                <div key={bouteille.id}>
+                    <h3>{bouteille.nom}</h3>
+                    <h3>{bouteille.millesime}</h3>
+                    <img src={bouteille.url_img} alt={bouteille?.nom} style={{width: '50px'}} />
+                    <p>{bouteille.pays}</p>
+                    <p>{bouteille.format}</p>
+                    <p>{bouteille.quantite}</p>
+                </div>
+            ))}
+        </Layout>
+    );
 };
 
 export default Cellier;
