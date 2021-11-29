f45c04b711f1ac5d42801b027d0a576bf594e3d4
Pier2208 <p.leroy.mtl@gmail.com>
2021-11-29 17:58:02 -0500

    reformat CellierBouteill component

--- resources/js/components/CellierBouteille.js
+++ resources/js/components/CellierBouteille.js
@@ -2,27 +2,30 @@ import React from "react";
 import { CellierBouteilleSection } from "./styles/CellierBouteilleSection.styled";
 import DummyBouteille from "../assets/svg/dummyBouteille.svg";
 
-const CellierBouteille = ({ nom, pays, url_img, format, prix_achat, millesime, quantite }) => {
-    return (
-        <CellierBouteilleSection>
+const CellierBouteille = ({
+    nom,
+    pays,
+    url_img,
+    format,
+    prix_achat,
+    millesime,
+    quantite,
+}) => (
+    <CellierBouteilleSection>
+        <div>
+            <h3>{nom}</h3>
+            <p>{millesime}</p>
+        </div>
+        <div>
+            <img src={url_img ? url_img : DummyBouteille} alt={nom} />
             <div>
-                <h3>{nom}</h3>
-                <p>{millesime}</p>
+                <p>{pays}</p>
+                <p>{format}</p>
+                <p>{prix_achat}$</p>
+                <p>{quantite}</p>
             </div>
-            <div>
-                <img
-                    src={url_img ? url_img : DummyBouteille}
-                    alt={nom}
-                />
-                <div>
-                    <p>{pays}</p>
-                    <p>{format}</p>
-                    <p>{prix_achat}$</p>
-                    <p>{quantite}</p>
-                </div>
-            </div>
-        </CellierBouteilleSection>
-    );
-};
+        </div>
+    </CellierBouteilleSection>
+);
 
 export default CellierBouteille;
