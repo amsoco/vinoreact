ecf4e4ab5e43188c39e1d9795c0dfec543a7395f
unknown <delphinecpicca@gmail.com>
2021-12-02 15:16:47 -0500

    modification input.js pour upload images

--- resources/js/components/Input.js
+++ resources/js/components/Input.js
@@ -6,18 +6,18 @@ import {Image} from 'cloudinary-react';
 
 const Input = () => {
     const [imageSelected, setImageSelected] = useState("");
+    const [imageUrl, setImageUrl] = useState("");
+    const [publicId, setPublicId] = useState("");
 
     const uploadImage = (files) => {
-        //console.log(files[0]);
         const formData = new FormData();
         formData.append("file", imageSelected);
         formData.append("upload_preset", "knxwwgoa");
 
         Axios.post("https://api.cloudinary.com/v1_1/vino-project/image/upload", 
             formData).then((response)=>{
-                console.log(response);
-                console.log(response.data.secure_url);
-                
+                setImageUrl(response.data.secure_url);
+                setPublicId(response.data.public_id);
             });
     };
 
@@ -29,8 +29,8 @@ const Input = () => {
 
                 <Image 
                 style={{ width: 100 }}
-                cloudName="vino-project" 
-                publicId="https://res.cloudinary.com/vino-project/image/upload/v1638310543/apaylzy0oeacsysjw27j.png"/>
+                cloudName="vino-project"
+                publicId={publicId}/>
             </div>
     )
 
