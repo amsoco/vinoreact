23338981ca5d462c3d972a1b1089ea8d0f2b0ce8
PaulAntoineGauvreau <paulantoine.gauvreau@gmail.com>
2021-12-06 10:41:10 -0500

    section fonctionne

--- resources/js/components/styles/CellierBouteilleSection.styled.js
+++ resources/js/components/styles/CellierBouteilleSection.styled.js
@@ -1,74 +1,62 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
 // la page cellier
 export const CellierBouteilleSection = styled.section`
-    margin: 0px 20px 0px 20px;
-    border-top: 1px solid #d8d8d8;
-    padding: 20px 10px 20px 10px;
-<<<<<<< HEAD
-    
+
+margin: 0px 20px 0px 20px;
+border-top: 1px solid #d8d8d8;
+padding: 20px 10px 20px 10px;
+
+& a {
     p {
         margin: 20px 0px 0px 0px;
-        font-size:1.44rem;
-        font-family: LincolnMillerBanner;
-        margin-left:auto;
+        font-size: 1.44rem;
+        font-family: "LincolnMillerBanner-Roman";
+        margin-left: auto;
     }
 
-    &>div {
-        display:flex;
+    & > div {
+        display: flex;
     }
-    &>div:last-child {
-=======
->>>>>>> upstream/main
-
-    & a {
-        p {
-            margin: 20px 0px 0px 0px;
-            font-size: 1.44rem;
-            font-family: "LincolnMillerBanner-Roman";
+    & > div:last-child {
+        div:last-child {
             margin-left: auto;
-        }
-
-        & > div {
-            display: flex;
-        }
-        & > div:last-child {
-            div:last-child {
-                margin-left: auto;
-                align-self: flex-end;
-            }
-        }
-
-        & > div > h3:first-child {
-            font-size: 1.44rem;
-            font-weight: 200;
-            max-width: 75%;
             align-self: flex-end;
         }
+    }
 
-        & > div > img {
-            height: 120px;
-            margin-top: 30px;
-        }
+    & > div > h3:first-child {
+        font-size: 1.44rem;
+        font-weight: 200;
+        max-width: 75%;
+        align-self: flex-end;
+    }
 
-        & > div > div > p {
-            font-size: 16px;
-            margin: 10px 0px 0px 0px;
-            //line-height:0;
-            text-align: right;
-        }
+    & > div > img {
+        height: 120px;
+        margin-top: 30px;
+    }
 
-    &>div>div>p:last-child {
-        color:#58111A;
-        font-family: Gotham Black;
+    & > div > div > p {
+        font-size: 16px;
+        margin: 10px 0px 0px 0px;
+        //line-height:0;
+        text-align: right;
+    }
+
+    & > div > div > p:last-child {
+        color: #58111a;
+        font-family: GothamBlack;
         font-weight: 100;
-        border: 2px solid #58111A;
+        border: 2px solid #58111a;
         padding: 4px;
         font-size: 0.833rem;
         border-radius: 4px;
-        width:fit-content;
-        margin-left:auto;
-        min-width:15px;
-        text-align:center;
+        width: fit-content;
+        margin-left: auto;
+        min-width: 15px;
+        text-align: center;
     }
-`;
+}
+
+`
