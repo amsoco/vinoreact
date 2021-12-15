f6e3211889d474142f990cb1f7541b23984b2eee
PaulAntoineGauvreau <paulantoine.gauvreau@gmail.com>
2021-12-15 09:54:40 -0500

    upstream

--- resources/js/components/RequireAuth.js
+++ resources/js/components/RequireAuth.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { useUser } from "../context/user";
-import { Navigate } from "react-router-dom";
+import { Navigate , useNavigate} from "react-router-dom";
+
 
 const RequireAuth = ({ children }) => {
     const { user } = useUser();
+    const navigate = useNavigate();
 
 
     /// l'admin a accès a tout les routes présentement ajustement si possible.
     // Par contre comme l'admin n'a pas de cellier c'est impossible de naviguer la l'application
+
     if (user === null) return null;
     return user ? (
         children
